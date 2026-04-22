50e232c88eeb0a3bb30e5fc47a3ab4167ec3be90
lanar191024 <zahraramadhani@student.plai.ac.id>
2026-04-22 11:14:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,3 @@ void displayHeader() {
     cout << "Status: Active Monitoring..." << endl;
     cout << "--------------------------------" << endl;
 }
-bool isSecure(string pwd) {
-    bool hasNum = false;
-    bool isLongEnough = (pwd.length() >= 8);
-    
-    for(char c : pwd) {
-        if(isdigit(c)) hasNum = true;
-    }
-    
-    return hasNum && isLongEnough;
-}
